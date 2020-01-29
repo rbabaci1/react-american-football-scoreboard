db3419426901036f80d7ddee13beb5edc79cec77
rabah babaci <4rabah@gmail.com>
2020-01-28 21:10:39 -0800

    render the states values for both teams

--- src/App.js
+++ src/App.js
@@ -4,27 +4,26 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(12);
-  const [awayScore, setAwayScore] = useState(12);
+  const [lionsScore, setLionsScore] = useState(11);
+  const [tigersScore, setTigersScore] = useState(11);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+            <div className="home__score">{lionsScore}</div>
           </div>
+
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
       </section>
+
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
