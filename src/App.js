4b15a1e225e31a9ea0fed07f70a2649ea6259741
rabah babaci <4rabah@gmail.com>
2020-01-29 17:06:56 -0800

    import the score board component to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import ScoreBoard from "./ScoreBoard";
 
 function App() {
   const [homeScore, setHomeScore] = useState(0);
@@ -9,18 +10,7 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-            <div className="home__score">{homeScore}</div>
-          </div>
-
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
+        <ScoreBoard scores={[homeScore, awayScore]} />
         <BottomRow />
       </section>
 
--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,2 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
+function ScoreBoard(props) {
+  return (
+    <div className="topRow">
+      <div className="home">
+        <h2 className="home__name">Lions</h2>
+        <div className="home__score">{props.scores[0]}</div>
+      </div>
+
+      <div className="timer">00:03</div>
+      <div className="away">
+        <h2 className="away__name">Tigers</h2>
+        <div className="away__score">{props.scores[1]}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ScoreBoard;
